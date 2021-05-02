774bd30cd78cb21664edffb552ef7c39ad8dc961
Ryan Epp <re17wj@brocku.ca>
2021-05-02 17:08:02 -0400

    Initial commit?

---
+++ Deli.GatherButton/src/Gather.cs
@@ -0,0 +1,18 @@
+﻿using Deli.Immediate;
+using Deli.Setup;
+using Deli.H3VR.Api;
+using FistVR;
+
+namespace Deli.GatherButton
+{
+	public class Gather : DeliBehaviour
+	{
+		public Gather()	{
+            WristMenu.RegisterWristMenuButton("Custom Button!", WristMenuButtonClicked);
+        }
+
+        private void WristMenuButtonClicked(FVRWristMenu wristMenu) {
+            Logger.LogMessage("Clicked!");
+        }
+    }
+}
