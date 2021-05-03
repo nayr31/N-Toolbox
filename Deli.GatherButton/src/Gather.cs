15f99f9c19696b4aa031ea510723cb75f80348f3
Ryan Epp <re17wj@brocku.ca>
2021-05-03 19:08:01 -0400

    Added more types, switched spawn->delete to transform object
    Left some whitelist code in there in case I feel like making it work, currently the guns weren't being gathered

--- Deli.GatherButton/src/Gather.cs
+++ Deli.GatherButton/src/Gather.cs
@@ -16,76 +16,82 @@ namespace Deli.GatherButton
 
         private void WristMenuButtonClicked(FVRWristMenu wristMenu)
         {
-            Logger.LogMessage("Gathering items...");
+            //Get array of objects 
+            FVRPhysicalObject[] objectArray = UnityEngine.Object.FindObjectsOfType<FVRPhysicalObject>();
             Vector3 playerPos = GM.CurrentPlayerBody.Head.position;
-            bool cont = false;//continuing variable to skip object deletion upon not gathering
-            //var hand = wristMenu.Hands[0].TouchSphere
-
-            //Guns
-            //FVRFireArm[] array = UnityEngine.Object.FindObjectsOfType<FVRFireArm>();
-            //for (int i = array.Length - 1; i >= 0; i--)	{
-            //	if (!array[i].IsHeld && array[i].QuickbeltSlot == null)	{
-            //		//Action
-            //		Logger.LogMessage("Guns...");
-            //	}
-            //}
-
-            //Sosiggun?
-            //SosigWeapon[] array2 = UnityEngine.Object.FindObjectsOfType<SosigWeapon>();
-            //for (int j = array2.Length - 1; j >= 0; j--)
+            //string[] whitelist =
             //{
-            //	if (!array2[j].O.IsHeld && array2[j].O.QuickbeltSlot == null && !array2[j].IsHeldByBot && !array2[j].IsInBotInventory)
-            //	{
-            //		UnityEngine.Object.Destroy(array2[j].gameObject);
-            //	}
+            //    "FVRFireArmMagazine",
+            //    "FVRFireArmRound",
+            //    "FVRFireArmClip",
+            //    "FVRFireArm",
+            //    "LAPD2019Battery",
+            //    "Molotov",
+            //    "Flashlight",
+            //    "FVRGrenade",
+            //    "FVRKnife",
+            //    "Speedloader",
+            //};
+
+
+            //foreach(var gatheredObject in FindObjectsOfType<FVRPhysicalObject>())
+            //{
+            //    if (refList(whitelist, gatheredObject.GetType().Name))
+            //        gatheredObject.transform.position = playerPos;
             //}
 
-            //Melee weapons
-            //FVRMeleeWeapon[] array3 = UnityEngine.Object.FindObjectsOfType<FVRMeleeWeapon>();
-            //for (int k = array3.Length - 1; k >= 0; k--) {
-            //	if (!array3[k].IsHeld && array3[k].QuickbeltSlot == null) {
-            //		UnityEngine.Object.Destroy(array3[k].gameObject);
-            //	}
-            //}
+            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRFireArm>())
+                if (!v.IsHeld && v.QuickbeltSlot == null)
+                    v.transform.position = playerPos;
 
-            //Magazines
-            Logger.LogMessage("Mags...");
-            cont = false;
-            FVRFireArmMagazine[] magArray = UnityEngine.Object.FindObjectsOfType<FVRFireArmMagazine>();
-            //--Spawn
-            try
-            {
-                for (int k = magArray.Length - 1; k >= 0; k--)
-                {
-                    if (!magArray[k].IsHeld && magArray[k].QuickbeltSlot == null)
-                    {
-                        UnityEngine.Object.Instantiate(magArray[k].gameObject, playerPos, Quaternion.identity);
-                    }
-                }
-                Logger.LogMessage("Mag gather done");
-                cont = true;
-            }
-            catch (Exception e)
-            {
-                Logger.LogMessage("Something happened when gathering mags:\n-- " + e);
-            }
+            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRFireArmMagazine>())
+                if (!v.IsHeld && v.QuickbeltSlot == null)
+                    v.transform.position = playerPos;
 
-            //--Destroy
-            if (cont)
-            {
-                try
-                {
-                    for (int k = magArray.Length - 1; k >= 0; k--)
-                    {
-                        UnityEngine.Object.Destroy(magArray[k].gameObject);
-                    }
-                    Logger.LogMessage("Mag destroy done");
-                }
-                catch (Exception e)
-                {
-                    Logger.LogMessage("Something happened when deleting excess mags:\n-- " + e);
-                }
-            }
+            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRFireArmRound>())
+                if (!v.IsHeld && v.QuickbeltSlot == null)
+                    v.transform.position = playerPos;
+
+            foreach (var v in UnityEngine.Object.FindObjectsOfType<Speedloader>())
+                if (!v.IsHeld && v.QuickbeltSlot == null)
+                    v.transform.position = playerPos;
+
+            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRFireArmClip>())
+                if (!v.IsHeld && v.QuickbeltSlot == null)
+                    v.transform.position = playerPos;
+
+            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRMeleeWeapon>())
+                if (!v.IsHeld && v.QuickbeltSlot == null)
+                    v.transform.position = playerPos;
+
+            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRGrenade>())
+                if (!v.IsHeld && v.QuickbeltSlot == null)
+                    v.transform.position = playerPos;
         }
+
+        //private bool refList(string[] list, string regex)
+        //{
+        //    foreach (string v in list)
+        //        if (v.Contains(regex))
+        //            return true;
+        //    return false;
+        //}
     }
 }
+//Logger.LogMessage("Gathering items...");
+            //try
+            //{
+            //    for (int k = objectArray.Length - 1; k >= 0; k--)
+            //    {
+            //        if (!objectArray[k].IsHeld && objectArray[k].QuickbeltSlot == null)
+            //        {
+            //            //UnityEngine.Object.Instantiate(magArray[k].gameObject, playerPos, Quaternion.identity);
+            //            objectArray[k].transform.position = playerPos;
+            //        }
+            //    }
+            //    Logger.LogMessage("Gathering completed");
+            //}
+            //catch (Exception e)
+            //{
+            //    Logger.LogMessage("Something happened while gatherings:\n-- " + e);
+            //}
