8ef9cba2b6cae494aec4b91e1bb4e95ac02637ed
Ryan Epp <re17wj@brocku.ca>
2021-05-04 19:45:01 -0400

    Optimizations
    - Removed redundant comments/code blocks
    - Cleaned up extra methods
    - Sent array outside to lower performance costs upon data creations
    - Moved physical object array get into foreach loop, wasn't being used anywhere else

--- Deli.GatherButton/src/Gather.cs
+++ Deli.GatherButton/src/Gather.cs
@@ -5,6 +5,7 @@ using FistVR;
 using UnityEngine;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Deli.GatherButton
 {
@@ -17,115 +18,34 @@ namespace Deli.GatherButton
 
         private void WristMenuButtonClicked(FVRWristMenu wristMenu)
         {
-            //Array of all objects
-            FVRPhysicalObject[] objectArray = UnityEngine.Object.FindObjectsOfType<FVRPhysicalObject>();
-            //Current player position to root random area
+            //Get player pos upon every button press
             Vector3 playerPos = GM.CurrentPlayerBody.Head.position;
-            //Moving vector between item spawns
-            Vector3 transformPos = playerPos;
-            //Whitelist of item types
-            Type[] whiteTypes =
-            {
-                typeof(FVRFireArm),
-                typeof(FVRFireArmMagazine),
-                typeof(Speedloader),
-                typeof(FVRFireArmRound),
-                typeof(FVRFireArmClip),
-                typeof(LAPD2019Battery),
-                typeof(Molotov),
-                typeof(FVRGrenade),
-                typeof(PinnedGrenade),
-                typeof(FVRKnife),
-                typeof(Flashlight),
-                typeof(FVRMeleeWeapon),
-            };
 
-            //Iterate through all objetcs, and transform them to an area around the player
-            foreach(var v in objectArray)
-                if(!v.IsHeld && v.QuickbeltSlot == null && refList(whiteTypes, v.GetType()))
-                    transformPos = action(v, transformPos, playerPos);
+            //Whitelisted object gather
+            foreach (var physObject in FindObjectsOfType<FVRPhysicalObject>())
+                if (!physObject.IsHeld && physObject.QuickbeltSlot == null && whiteTypes.Contains(physObject.GetType()))
+                    physObject.transform.position = playerPos + UnityEngine.Random.insideUnitSphere;
 
-            
-            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRFireArm>())
-                if (!v.IsHeld && v.QuickbeltSlot == null)
-                    transformPos = action(v, transformPos, playerPos);
-            /*
-            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRFireArmMagazine>())
-                if (!v.IsHeld && v.QuickbeltSlot == null)
-                    transformPos = action(v, transformPos, playerPos);
-
-            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRFireArmRound>())
-                if (!v.IsHeld && v.QuickbeltSlot == null)
-                    transformPos = action(v, transformPos, playerPos);
-
-            foreach (var v in UnityEngine.Object.FindObjectsOfType<Speedloader>())
-                if (!v.IsHeld && v.QuickbeltSlot == null)
-                    transformPos = action(v, transformPos, playerPos);
-
-            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRFireArmClip>())
-                if (!v.IsHeld && v.QuickbeltSlot == null)
-                    transformPos = action(v, transformPos, playerPos);
-
-            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRMeleeWeapon>())
-                if (!v.IsHeld && v.QuickbeltSlot == null)
-                    transformPos = action(v, transformPos, playerPos);
-
-            foreach (var v in UnityEngine.Object.FindObjectsOfType<FVRGrenade>())
-                if (!v.IsHeld && v.QuickbeltSlot == null)
-                    transformPos = action(v, transformPos, playerPos);
-
-            foreach (var v in UnityEngine.Object.FindObjectsOfType<PinnedGrenade>())
-                if (!v.IsHeld && v.QuickbeltSlot == null)
-                    transformPos = action(v, transformPos, playerPos);*/
+            //Gun gather since its missing from the phys object gather
+            foreach (var physObject in FindObjectsOfType<FVRFireArm>())
+                if (!physObject.IsHeld && physObject.QuickbeltSlot == null)
+                    physObject.transform.position = playerPos + UnityEngine.Random.insideUnitSphere;
         }
 
-        //Preforms the move action on the object, then returns an updated position for a random space around the player
-        private Vector3 action(FVRPhysicalObject v, Vector3 transformPos, Vector3 playerPos)
+        static readonly Type[] whiteTypes =
         {
-            v.transform.position = transformPos;
-            return rotTrans(transformPos, playerPos);
-        }
-
-        //Changes a vector to a random space around a second vector
-        private Vector3 rotTrans(Vector3 transformPos, Vector3 playerPos)
-        {
-            var tempVect3 = transformPos;
-            var offsetX = UnityEngine.Random.Range(-1f, 1f);
-            var offsetY = UnityEngine.Random.Range(-1f, 0f);//up-down
-            var offsetZ = UnityEngine.Random.Range(-1f, 1f);
-
-            tempVect3 = new Vector3(playerPos.x + offsetX, playerPos.y + offsetY, playerPos.z + offsetZ);
-
-            return tempVect3;
-        }
-
-
-        //private bool refList(string[] list, string regex)
-        //{
-        //    foreach (string v in list)
-        //        if (v.Contains(regex))
-        //            return true;
-        //    return false;
-        //}
-
-        //Compares a list of types to see if a type is contained within it
-        private bool refList(Type[] list, Type regex)
-        {
-            foreach (Type v in list)
-                if (v.Equals(regex))
-                    return true;
-            return false;
-        }
+            typeof(FVRFireArm),
+            typeof(FVRFireArmMagazine),
+            typeof(Speedloader),
+            typeof(FVRFireArmRound),
+            typeof(FVRFireArmClip),
+            typeof(LAPD2019Battery),
+            typeof(Molotov),
+            typeof(FVRGrenade),
+            typeof(PinnedGrenade),
+            typeof(FVRKnife),
+            typeof(Flashlight),
+            typeof(FVRMeleeWeapon),
+        };
     }
-
-
 }
-//Whitelist code:
-
-
-
-//foreach(var gatheredObject in FindObjectsOfType<FVRPhysicalObject>())
-//{
-//    if (refList(whitelist, gatheredObject.GetType().Name))
-//        gatheredObject.transform.position = playerPos;
-//}
