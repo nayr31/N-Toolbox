3cb788ffb4d1b19c9afb7350c742abc3d9c0052c
Ryan Epp <re17wj@brocku.ca>
2021-05-07 20:32:35 -0400

    Fixed weapons not being scaled in unit area

--- Deli.NToolbox/src/NToolbox.cs
+++ Deli.NToolbox/src/NToolbox.cs
@@ -31,7 +31,8 @@ namespace Deli.NToolbox
             //Gun gather since its missing from the phys object gather
             foreach (var physObject in FindObjectsOfType<FVRFireArm>())
                 if (!physObject.IsHeld && physObject.QuickbeltSlot == null)
-                    physObject.transform.position = playerPos + UnityEngine.Random.insideUnitSphere;
+                    physObject.transform.position = playerPos +
+                        Vector3.Scale(UnityEngine.Random.insideUnitSphere, new Vector3(1.3f, 0.7f, 1.3f)) - new Vector3(0, 0.5f, 0);
         }
 
         private void ResetTrapsButtonClicked(FVRWristMenu wristMenu)
